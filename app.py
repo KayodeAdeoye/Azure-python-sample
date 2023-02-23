5d07f4d75819d8b23641cd62b35ce27c9c324d0f
Kayode Adeoye <kayode.adeoye@outlook.com>
2023-02-23 04:51:33 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
-from datetime import datetime
-from flask import Flask, render_template, request, redirect, url_for, send_from_directory
+from flask import Flask
+
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello, World!"
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+if __name__ == '__main__':
+    app.run()
